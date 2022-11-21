8e6a101492a43f0778edacedca7e22dec307f3b8
bryansomto <bryansomto@gmail.com>
2022-11-21 04:50:47 +0100

    front end improvement

--- website/views.py
+++ website/views.py
@@ -1,10 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
-@login_required
+@views.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        return redirect(url_for('views.nginx_config'))
     return render_template("home.html", user=current_user)
+
+@views.route('/nginx-config')
+@login_required
+def nginx_config():
+    return render_template("nginx-config.html", user=current_user)
