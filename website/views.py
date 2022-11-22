5f4034cf9325b44b4ada202cc118ed0f13753a2a
bryansomto <bryansomto@gmail.com>
2022-11-22 04:48:42 +0100

    extra modif

--- website/views.py
+++ website/views.py
@@ -10,7 +10,13 @@ def home():
         return redirect(url_for('views.nginx_config'))
     return render_template("home.html", user=current_user)
 
-@views.route('/nginx-config')
+@views.route('/nginx-config', methods=['GET', 'POST'])
 @login_required
 def nginx_config():
+    if request.method == 'POST':
+        server_name = request.form.get('server_name')
+        ip_addr = request.form.get('ip_addr')
+        id_file = request.form['id_file']
+        
+        f = open(id_file, "r", encoding="UTF-8")
     return render_template("nginx-config.html", user=current_user)
